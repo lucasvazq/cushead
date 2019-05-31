281a5f4ba680cc8e3ea7bea8c9bf61af6ae4a5b4
These <lucas5zvazquez@gmail.com>
2019-05-31 16:09:41 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='cushead',  
+    version='2.1.1',
+    scripts=['cushead.py'],
+    author="Lucas Vazquez",
+    author_email="lucas5zvazquez@gmail.com",
+    description="Improves your SEO and the UX generating custom head elements.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrsantos321/cushead",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
