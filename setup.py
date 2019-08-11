419242ac525464ef8ace762855baf9098944b173
mrsantosm321 <lucas5zvazquez@gmail.com>
2019-08-10 23:34:21 -0300

    new version ready

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 setuptools.setup(
     name='cushead.py',
-    version='2.3.1',
     scripts=['cushead.py'],
-    install_requires=[
-        'argparse'
-    ],
+    version='2.3.1',
+    url='https://github.com/lucasvazq/cushead.py',
+    python_requires='>=3.5, <4',
+    packages=setuptools.find_packages(),
+    install_requires=['argparse'],
     author='Lucas Vazquez',
     author_email='lucas5zvazquez@gmail.com',
     description=
@@ -22,15 +23,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     keywords='SEO, UX, front-end',
-    url='https://github.com/lucasvazq/cushead.py',
-    packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.5, <4',
-    install_requires=['argparse']
+    ]
 )
