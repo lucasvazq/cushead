ede27c359ea60c7c579692d20593b0806c704bb7
mrsantosm321 <lucas5zvazquez@gmail.com>
2019-10-05 16:52:16 -0300

    fix \module/ f

--- src/module/__main__.py
+++ src/module/__main__.py
@@ -4,10 +4,12 @@
 """Main module of the package module
 
 Classes:
-    DefaultConfig
+    Config
     Files
-    Main(dict, dict, str)
+    Main
 """
+
+from typing import Union
 
 from src.info import Info
 from src.module.files.complementary_files import ComplementaryFilesCreation
@@ -15,7 +17,6 @@ from src.module.files.base import BaseFileCreation
 from src.module.config.user import UserConfigHandler
 from src.module.config.images import IconsFormatConfig
 from src.module.files.images import ImageFilesCreation
-
 from src.services.logs import Logs, SpecialMessages
 
 
@@ -28,7 +29,7 @@ class Files(BaseFileCreation, ComplementaryFilesCreation, ImageFilesCreation):
     """Class used to handle the generation of files
 
     Methods:
-        all_files -> dict
+        all_files
     """
     def all_files(self) -> dict:
         """Return a dict with the structure of all files"""
@@ -41,13 +42,15 @@ class Main(Config, Files, UserConfigHandler, Logs, SpecialMessages):
     The entire sub-modules serve this class
 
     Init:
-        icons_config dict = {}: icons format settings
-        user_config dict = {}: user settings
+        icons_config Union[dict, None] = None: icons format 
+        user_config Union[dict, None] = None: user settings
         output_path str = '': output folder
     """
     info = Info.get_info()
 
-    def __init__(self, icons_config: dict = {}, user_config: dict = {},
+    def __init__(self,
+                 icons_config: Union[dict, None] = None,
+                 user_config: Union[dict, None] = None,
                  main_path: str = ''):
         self.config = self.transform(user_config, main_path)
         self.icons_config = icons_config or self.default_icons_config()
