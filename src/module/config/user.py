9e5db37b09fe3d9d5b935815234cceaeb969af44
Restyled.io <commits@restyled.io>
2019-10-06 00:29:24 +0000

    Restyled by yapf

--- src/module/config/user.py
+++ src/module/config/user.py
@@ -32,20 +32,21 @@ class DefaultUserConfig:
     def default_images() -> List[Dict[str, str]]:
         """Generate images files to attach to the preset settings"""
         binary_files = []
-        realpath = path.join(path.dirname(path.realpath(__file__)), "../../assets")
+        realpath = path.join(path.dirname(path.realpath(__file__)),
+                             "../../assets")
         image_files = Images.images_list()
         for filename in image_files:
             filepath = path.join(realpath, filename)
             with open(filepath, "rb") as binary_file:
-                binary_files.append(
-                    {"filename": filename, "content": binary_file.read()}
-                )
+                binary_files.append({
+                    "filename": filename,
+                    "content": binary_file.read()
+                })
         return binary_files
 
     def default_settings(self) -> str:
         """Generate config file in indented JSON format"""
-        settings = textwrap.dedent(
-            f"""\
+        settings = textwrap.dedent(f"""\
             {{
                 'comment':  {{
                     'About':            'Config file used by python CUSHEAD',
@@ -110,16 +111,15 @@ class DefaultUserConfig:
                         }}
                     ]
                 }}
-            }}"""
-        )
+            }}""")
         settings = settings.replace("'", '"')
         return settings
 
 
 class UserConfigHandler(Logs):
-    def transform(
-        self, user_settings: Union[dict, None] = None, main_path: str = ""
-    ) -> dict:
+    def transform(self,
+                  user_settings: Union[dict, None] = None,
+                  main_path: str = "") -> dict:
         """Format default settings to a dict for this package classes
 
         Format default settings dictionary into a dictionary that the classes
@@ -143,9 +143,8 @@ class UserConfigHandler(Logs):
         social_media = default.get("social_media", {})
         progressive_web_app = settings.get("progressive_web_apps", {})
         if "required" not in settings:
-            self.error_log(
-                "Miss 'required' object and it's required in " "config file."
-            )
+            self.error_log("Miss 'required' object and it's required in "
+                           "config file.")
         settings = {
             **settings["required"],
             **recommended,
@@ -163,13 +162,8 @@ class UserConfigHandler(Logs):
             if validate:
                 self.error_log(validate)
 
-        a = (
-            11111111111111111111111111111111
-            + 11111111111111111111111111
-            + 1111111111111111111
-            + 1111111111111111111111
-            + """wasa"""
-        )
+        a = (11111111111111111111111111111111 + 11111111111111111111111111 +
+             1111111111111111111 + 1111111111111111111111 + """wasa""")
 
         # Sanitize static_url key
         # Prevent:
@@ -186,22 +180,16 @@ class UserConfigHandler(Logs):
         # Define the main path as the passed throught -file argument
         settings["main_folder_path"] = main_path
         settings["output_folder_path"] = path.join(
-            settings["main_folder_path"], "output"
-        )
+            settings["main_folder_path"], "output")
         settings["static_folder_path"] = path.join(
-            settings["output_folder_path"], settings["static_folder_path"]
-        )
+            settings["output_folder_path"], settings["static_folder_path"])
 
-        settings["favicon_ico"] = path.join(
-            settings["main_folder_path"], settings["favicon_ico"]
-        )
-        settings["favicon_png"] = path.join(
-            settings["main_folder_path"], settings["favicon_png"]
-        )
-        settings["favicon_svg"] = path.join(
-            settings["main_folder_path"], settings["favicon_svg"]
-        )
-        settings["preview_png"] = path.join(
-            settings["main_folder_path"], settings["preview_png"]
-        )
+        settings["favicon_ico"] = path.join(settings["main_folder_path"],
+                                            settings["favicon_ico"])
+        settings["favicon_png"] = path.join(settings["main_folder_path"],
+                                            settings["favicon_png"])
+        settings["favicon_svg"] = path.join(settings["main_folder_path"],
+                                            settings["favicon_svg"])
+        settings["preview_png"] = path.join(settings["main_folder_path"],
+                                            settings["preview_png"])
         return settings
