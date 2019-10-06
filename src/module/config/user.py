74a94f9b2a2f84a744cacf10cf915c3ace23fc12
Restyled.io <commits@restyled.io>
2019-10-06 00:29:22 +0000

    Restyled by black

--- src/module/config/user.py
+++ src/module/config/user.py
@@ -25,29 +25,27 @@ class DefaultUserConfig:
         @staticmethod default_images
         default_settings
     """
+
     info = Info.get_info()
 
     @staticmethod
     def default_images() -> List[Dict[str, str]]:
         """Generate images files to attach to the preset settings"""
         binary_files = []
-        realpath = path.join(path.dirname(path.realpath(__file__)),
-                             "../../assets")
+        realpath = path.join(path.dirname(path.realpath(__file__)), "../../assets")
         image_files = Images.images_list()
         for filename in image_files:
             filepath = path.join(realpath, filename)
-            with open(filepath, 'rb') as binary_file:
+            with open(filepath, "rb") as binary_file:
                 binary_files.append(
-                    {
-                        'filename': filename,
-                        'content': binary_file.read(),
-                    }
+                    {"filename": filename, "content": binary_file.read()}
                 )
         return binary_files
 
     def default_settings(self) -> str:
         """Generate config file in indented JSON format"""
-        settings = textwrap.dedent(f"""\
+        settings = textwrap.dedent(
+            f"""\
             {{
                 'comment':  {{
                     'About':            'Config file used by python CUSHEAD',
@@ -112,16 +110,16 @@ class DefaultUserConfig:
                         }}
                     ]
                 }}
-            }}""")
-        settings = settings.replace('\'', '"')
+            }}"""
+        )
+        settings = settings.replace("'", '"')
         return settings
 
 
 class UserConfigHandler(Logs):
-
-    def transform(self,
-                  user_settings: Union[dict, None] = None,
-                  main_path: str = '') -> dict:
+    def transform(
+        self, user_settings: Union[dict, None] = None, main_path: str = ""
+    ) -> dict:
         """Format default settings to a dict for this package classes
 
         Format default settings dictionary into a dictionary that the classes
@@ -138,66 +136,72 @@ class UserConfigHandler(Logs):
         settings = user_settings or {}
 
         # Construct config
-        recommended = settings.get('recommended', {})
-        default = settings.get('default', {})
-        general = default.get('general', {})
-        basic = default.get('basic', {})
-        social_media = default.get('social_media', {})
-        progressive_web_app = settings.get('progressive_web_apps', {})
-        if 'required' not in settings:
-            self.error_log("Miss 'required' object and it's required in "
-                           "config file.")
-        settings = {**settings['required'], **recommended, **general,
-                    **basic, **social_media, **progressive_web_app}
+        recommended = settings.get("recommended", {})
+        default = settings.get("default", {})
+        general = default.get("general", {})
+        basic = default.get("basic", {})
+        social_media = default.get("social_media", {})
+        progressive_web_app = settings.get("progressive_web_apps", {})
+        if "required" not in settings:
+            self.error_log(
+                "Miss 'required' object and it's required in " "config file."
+            )
+        settings = {
+            **settings["required"],
+            **recommended,
+            **general,
+            **basic,
+            **social_media,
+            **progressive_web_app,
+        }
 
         # Required values
-        required_values = ['static_url']
+        required_values = ["static_url"]
         for key in required_values:
             validator = KeysValidator(dictionary=settings, key=key)
             validate = validator.key_exists()
             if validate:
                 self.error_log(validate)
 
-        a = 11111111111111111111111111111111 + 11111111111111111111111111 + \
-            1111111111111111111 + 1111111111111111111111 + """wasa"""
+        a = (
+            11111111111111111111111111111111
+            + 11111111111111111111111111
+            + 1111111111111111111
+            + 1111111111111111111111
+            + """wasa"""
+        )
 
         # Sanitize static_url key
         # Prevent:
         #   output = /output/
         #   static_url = /static/
         #   output + static_url = /static/ [root/static/]
-        if settings['static_url'][0] == '/':
-            settings['static_folder_path'] = settings['static_url'][1:]
+        if settings["static_url"][0] == "/":
+            settings["static_folder_path"] = settings["static_url"][1:]
         # HEre, prevent //
-        if settings['static_url'][-1] == '/':
-            settings['static_url'] = settings['static_url'][:-1]
+        if settings["static_url"][-1] == "/":
+            settings["static_url"] = settings["static_url"][:-1]
 
         # Make paths
         # Define the main path as the passed throught -file argument
-        settings['main_folder_path'] = main_path
-        settings['output_folder_path'] = path.join(
-            settings['main_folder_path'],
-            'output'
+        settings["main_folder_path"] = main_path
+        settings["output_folder_path"] = path.join(
+            settings["main_folder_path"], "output"
         )
-        settings['static_folder_path'] = path.join(
-            settings['output_folder_path'],
-            settings['static_folder_path']
+        settings["static_folder_path"] = path.join(
+            settings["output_folder_path"], settings["static_folder_path"]
         )
 
-        settings['favicon_ico'] = path.join(
-            settings['main_folder_path'],
-            settings['favicon_ico'],
+        settings["favicon_ico"] = path.join(
+            settings["main_folder_path"], settings["favicon_ico"]
         )
-        settings['favicon_png'] = path.join(
-            settings['main_folder_path'],
-            settings['favicon_png'],
+        settings["favicon_png"] = path.join(
+            settings["main_folder_path"], settings["favicon_png"]
         )
-        settings['favicon_svg'] = path.join(
-            settings['main_folder_path'],
-            settings['favicon_svg'],
+        settings["favicon_svg"] = path.join(
+            settings["main_folder_path"], settings["favicon_svg"]
         )
-        settings['preview_png'] = path.join(
-            settings['main_folder_path'],
-            settings['preview_png'],
+        settings["preview_png"] = path.join(
+            settings["main_folder_path"], settings["preview_png"]
         )
         return settings
