78a9487f784ad6a34c2c5f8821fb4d85c7524138
= <=>
2020-01-12 15:19:18 -0300

    Searching invalid syntax

--- src_2/helpers.py
+++ src_2/helpers.py
@@ -16,7 +16,16 @@ def indent_dict(dictionary, base_indentation_level):
         )
     indented_content = ",\n".join(indented_content)
     """
-    indented_content = ",\n".join([f"{INDENTATION * (base_indentation_level + 1)}'{key}': '{value}'" for key, value in dictionary])}
+    #indented_content = ",\n".join([f"{INDENTATION * (base_indentation_level + 1)}'{key}': '{value}'" for key, value in dictionary])}
+
+    indented_content = []
+    for key, value in dictionary:
+        indentation = INDENTATION * (base_indentation_level + 1)
+        indented_content.append(
+            f"{indentation}'{key}': '{value}'"
+        )
+    indented_content = ",\n".join(indented_content)
+
     return (
         f"{INDENTATION * base_indentation_level}{{\n"
         f"{indented_content}\n"
