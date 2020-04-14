08fb105c6a96caa70687ca182e70505a03054869
Restyled.io <commits@restyled.io>
2020-04-14 02:30:53 +0000

    Restyled by black

--- src/info.py
+++ src/info.py
@@ -7,22 +7,27 @@ __package_name__ = "cushead"
 __package_version__ = "3.1.8"
 __source__ = "https://github.com/lucasvazq/{}".format(__package_name__)
 __documentation__ = "https://github.com/lucasvazq/{}/blob/master/README.md".format(
-    __package_name__)
+    __package_name__
+)
 __python_min_version__ = (3, 5)
 __python_max_version__ = (4, 0)
 __required_packages__ = ["python-resize-image", "Pillow"]
 __author__ = "Lucas Vazquez"
 __author_page__ = "https://github.com/lucasvazq"
 __email__ = "lucas5zvazquez@gmail.com"
-__description__ = ("CLI that help you to improve the SEO and UX of your "
-                   "websites.")
+__description__ = (
+    "CLI that help you to improve the SEO and UX of your " "websites."
+)
 __license__ = "MIT"
-__keywords__ = ("SEO, meta-tags, UX, PWA, front-end, favicon, manifest, " +
-                "robots, browserconfig, sitemap, opensearch")
+__keywords__ = (
+    "SEO, meta-tags, UX, PWA, front-end, favicon, manifest, "
+    + "robots, browserconfig, sitemap, opensearch"
+)
 
 # Unused
 __copyright__ = "{} © 2020 {}. Released under the MIT License.".format(
-    __package_name__, __author__)
+    __package_name__, __author__
+)
 __maintainer__ = "Lucas Vazquez"
 __status__ = "Production"
 
