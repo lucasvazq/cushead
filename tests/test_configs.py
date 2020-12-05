9b3843143a8b193b3dfe33b29d743b29fc1ee271
Restyled.io <commits@restyled.io>
2020-12-05 11:41:05 +0000

    Restyled by autopep8

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -24,26 +24,32 @@ class TestConfig(base_tests.BaseTests):
 
         # Compare the folders structure.
         self.assertEqual(
-            [str(file.relative_to(self.output_folder)) for file in self.output_folder.rglob("*")],
-            [str(file.relative_to(template_folder)) for file in template_folder.rglob("*")],
+            [str(file.relative_to(self.output_folder))
+             for file in self.output_folder.rglob("*")],
+            [str(file.relative_to(template_folder))
+             for file in template_folder.rglob("*")],
         )
 
         # Compare the files.
         for generated_file in self.output_folder.rglob("*"):
-            template_file = template_folder / str(generated_file.relative_to(self.output_folder))
+            template_file = template_folder / \
+                str(generated_file.relative_to(self.output_folder))
             if generated_file.is_dir():
                 self.assertTrue(template_file.is_dir())
             elif generated_file.suffix in (".png", ".ico"):
-                self.assertEqual(generated_file.read_bytes(), template_file.read_bytes())
+                self.assertEqual(generated_file.read_bytes(),
+                                 template_file.read_bytes())
             else:
-                self.assertEqual(generated_file.read_text(), template_file.read_text())
+                self.assertEqual(generated_file.read_text(),
+                                 template_file.read_text())
 
     def test_default_config(self) -> None:
         """
         Test the default config.
         """
         self.execute_cli(args=["-c", str(self.config_file)])
-        self.compare_output(template_folder_path=pathlib.Path("default_config"))
+        self.compare_output(
+            template_folder_path=pathlib.Path("default_config"))
 
     def test_missing_keys(self) -> None:
         """
@@ -84,7 +90,8 @@ class TestConfig(base_tests.BaseTests):
         del self.config["background_color"]
         self.write_config_file()
         self.execute_cli(args=["-c", str(self.config_file)])
-        self.compare_output(template_folder_path=pathlib.Path("custom_key_values/background_color"))
+        self.compare_output(template_folder_path=pathlib.Path(
+            "custom_key_values/background_color"))
 
         self.tearDown()
         self.setUp()
@@ -93,7 +100,8 @@ class TestConfig(base_tests.BaseTests):
         del self.config["title"]
         self.write_config_file()
         self.execute_cli(args=["-c", str(self.config_file)])
-        self.compare_output(template_folder_path=pathlib.Path("custom_key_values/title"))
+        self.compare_output(template_folder_path=pathlib.Path(
+            "custom_key_values/title"))
 
         self.tearDown()
         self.setUp()
@@ -102,7 +110,8 @@ class TestConfig(base_tests.BaseTests):
         self.config["static_url"] = "https://cdn.com"
         self.write_config_file()
         self.execute_cli(args=["-c", str(self.config_file)])
-        self.compare_output(template_folder_path=pathlib.Path("custom_key_values/static_url"))
+        self.compare_output(template_folder_path=pathlib.Path(
+            "custom_key_values/static_url"))
 
     def test_exmaple_config(self) -> None:
         """
@@ -111,7 +120,8 @@ class TestConfig(base_tests.BaseTests):
         self.config["google_tag_manager"] = "GTM-NGGH6LK"
         self.write_config_file()
         self.execute_cli(args=["-c", str(self.config_file)])
-        self.compare_output(template_folder_path=pathlib.Path("../../docs/example/dist"))
+        self.compare_output(template_folder_path=pathlib.Path(
+            "../../docs/example/dist"))
 
 
 if __name__ == "__main__":
