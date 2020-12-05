671923f3af0c5df443680ddcdba33ad50c35afcd
Restyled.io <commits@restyled.io>
2020-12-05 03:59:21 +0000

    Restyled by clang-format

--- tests/templates/custom_key_values/static_url/sw.js
+++ tests/templates/custom_key_values/static_url/sw.js
@@ -1,8 +1,9 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js");
 
 workbox.setConfig({
-  skipWaiting: true,
-  clientsClaim: true,
+  skipWaiting : true,
+  clientsClaim : true,
 });
 
 var CacheFirst = workbox.strategies.CacheFirst;
@@ -14,55 +15,45 @@ var precacheAndRoute = workbox.precaching.precacheAndRoute;
 var CacheableResponsePlugin = workbox.cacheableResponse.CacheableResponsePlugin;
 
 // Cache images.
-registerRoute(
-  /\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/,
-  new CacheFirst({
-    cacheName: "images",
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 60,
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-      }),
-    ],
-  })
-);
+registerRoute(/\.(?:png|gif|jpg|jpeg|webp|svg|ico)$/, new CacheFirst({
+                cacheName : "images",
+                plugins : [
+                  new ExpirationPlugin({
+                    maxEntries : 60,
+                    maxAgeSeconds : 60 * 60 * 24 * 30,
+                  }),
+                ],
+              }));
 
 // Cache Google Fonts stylesheets.
-registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  new StaleWhileRevalidate({
-    cacheName: "google-fonts-stylesheets",
-  })
-);
+registerRoute(/^https:\/\/fonts\.googleapis\.com/, new StaleWhileRevalidate({
+                cacheName : "google-fonts-stylesheets",
+              }));
 
 // Cache Google Fonts webfont files.
-registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  new CacheFirst({
-    cacheName: "google-fonts-webfonts",
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-      new ExpirationPlugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-      }),
-    ],
-  })
-);
+registerRoute(/^https:\/\/fonts\.gstatic\.com/, new CacheFirst({
+                cacheName : "google-fonts-webfonts",
+                plugins : [
+                  new CacheableResponsePlugin({
+                    statuses : [ 0, 200 ],
+                  }),
+                  new ExpirationPlugin({
+                    maxAgeSeconds : 60 * 60 * 24 * 365,
+                  }),
+                ],
+              }));
 
 // Cache js and css files.
 registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate());
 
 // Cache URLs.
 precacheAndRoute(
-  [
+    [
+      {
+        url : "/index.html",
+        revision : "9aa77b",
+      },
+    ],
     {
-      url: "/index.html",
-      revision: "9aa77b",
-    },
-  ],
-  {
-    cleanUrls: true,
-  }
-);
+      cleanUrls : true,
+    });
