0fc1c88c97ea5d64ab66b139ac8a27c6d5902504
lucasvazq <lucas5zvazquez@gmail.com>
2020-12-05 08:40:53 -0300

    improve: off some assertions and format code with prettier

--- docs/example/.lighthouserc.js
+++ docs/example/.lighthouserc.js
@@ -1,13 +1,17 @@
 module.exports = {
   ci: {
     collect: {
-        staticDistDir: "docs/example/dist",
-        settings: {
-            skipAudits: ["redirects-http"],
-        },
+      staticDistDir: "docs/example/dist",
+      settings: {
+        skipAudits: ["redirects-http"],
+      },
     },
     assert: {
       preset: "lighthouse:all",
+      assertions: {
+        "long-tasks": "off",
+        "redirects-http": "off",
+      },
     },
     upload: {
       target: "temporary-public-storage",
