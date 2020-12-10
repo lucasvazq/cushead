119dfae326d0026bf571e8381b1741ee52e64cbd
lucasvazq <lucas5zvazquez@gmail.com>
2020-12-10 20:55:43 -0300

    🎨 remove unnecesary type declaration

--- tests/base_tests.py
+++ tests/base_tests.py
@@ -9,7 +9,6 @@ import pathlib
 import shutil
 import unittest
 from typing import Any
-from typing import Dict
 from typing import List
 from typing import Optional
 
@@ -34,7 +33,7 @@ class BaseTests(unittest.TestCase):
         self.config_file = self.config_folder / "config.json"
         self.output_folder = self.config_folder / "output"
         self.usage = setup.get_parser().usage
-        self.config: Dict[str, Any] = {}
+        self.config = {}
 
     @staticmethod
     def _execute_cli_silently(*, args: List[str]) -> None:
