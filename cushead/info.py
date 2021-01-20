3ab83922189c0ee96a6420c648f1743584782148
lucasvazq <lucas5zvazquez@gmail.com>
2021-01-20 00:55:21 -0300

    🔖 v4.0.4

--- cushead/info.py
+++ cushead/info.py
@@ -4,7 +4,7 @@ Store package information.
 import pathlib
 
 PACKAGE_NAME = "cushead"
-PACKAGE_VERSION = "4.0.3"
+PACKAGE_VERSION = "4.0.4"
 PYTHON_MIN_VERSION = (3, 8)
 SOURCE = f"https://github.com/lucasvazq/{PACKAGE_NAME}"
 DOCUMENTATION = f"https://github.com/lucasvazq/{PACKAGE_NAME}/blob/master/README.md"
@@ -14,10 +14,3 @@ DESCRIPTION = "CLI that help you to improve the SEO and UX of your websites."
 PACKAGE_LICENSE = "MIT"
 KEYWORDS = "SEO, meta-tags, UX, PWA, front-end, favicon, manifest, robots, browserconfig, sitemap, opensearch"
 AUTHOR_PAGE = "https://github.com/lucasvazq"
-
-if (pathlib.Path(__file__).parent / "requirements.txt").exists():
-    # Used in build context.
-    REQUIRED_PACKAGES = (pathlib.Path(__file__).parent / "requirements.txt").read_text().split()
-else:
-    # Used in develop and testing context.
-    REQUIRED_PACKAGES = (pathlib.Path(__file__).parent.parent / "requirements.txt").read_text().split()
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Setup script.
+Host the setup function.
 """
 import pathlib
 
@@ -8,51 +8,61 @@ import setuptools
 
 from cushead import info
 
-_ASSETS_PATH = pathlib.Path(info.PACKAGE_NAME) / "console" / "assets" / "images"
-_TEMPLATES_PATH = pathlib.Path(info.PACKAGE_NAME) / "generator" / "templates" / "jinja" / "templates"
 
-setuptools.setup(
-    name=info.PACKAGE_NAME,
-    version=info.PACKAGE_VERSION,
-    entry_points={"console_scripts": [f"{info.PACKAGE_NAME}={info.PACKAGE_NAME}.console.console:main"]},
-    url=info.SOURCE,
-    project_urls={
-        "Documentation": info.DOCUMENTATION,
-        "Source": info.SOURCE,
-    },
-    python_requires=f">={info.PYTHON_MIN_VERSION[0]}.{info.PYTHON_MIN_VERSION[1]}",
-    packages=setuptools.find_packages(exclude=("tests",)),
-    data_files=[
-        (info.PACKAGE_NAME, ["requirements.txt", "LICENSE.md", "README.md"]),
-        (_ASSETS_PATH, [str(file) for file in pathlib.Path(_ASSETS_PATH).iterdir()]),
-        (_TEMPLATES_PATH, [str(file) for file in pathlib.Path(_TEMPLATES_PATH).iterdir()]),
-    ],
-    zip_safe=False,
-    install_requires=info.REQUIRED_PACKAGES,
-    author=info.AUTHOR,
-    author_email=info.EMAIL,
-    description=info.DESCRIPTION,
-    long_description=pathlib.Path("README.md").read_text(),
-    long_description_content_type="text/markdown",
-    license=info.PACKAGE_LICENSE,
-    keywords=info.KEYWORDS,
-    platforms="any",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Utilities",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Code Generators",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: User Interfaces",
-    ],
-)
+def setup():
+    """
+    Execute the setup.
+    """
+    assets_path = pathlib.Path(info.PACKAGE_NAME) / "console" / "assets" / "images"
+    templates_path = pathlib.Path(info.PACKAGE_NAME) / "generator" / "templates" / "jinja" / "templates"
+
+    setuptools.setup(
+        name=info.PACKAGE_NAME,
+        version=info.PACKAGE_VERSION,
+        entry_points={"console_scripts": [f"{info.PACKAGE_NAME}={info.PACKAGE_NAME}.console.console:main"]},
+        url=info.SOURCE,
+        project_urls={
+            "Documentation": info.DOCUMENTATION,
+            "Source": info.SOURCE,
+        },
+        python_requires=f">={info.PYTHON_MIN_VERSION[0]}.{info.PYTHON_MIN_VERSION[1]}",
+        packages=setuptools.find_packages(exclude=(file.name for file in pathlib.Path("").iterdir() if file.name != info.PACKAGE_NAME)),
+        include_package_data=True,
+        data_files=[
+            ("", ["requirements.txt", "LICENSE.md", "README.md"]),
+            (assets_path, [str(file) for file in pathlib.Path(assets_path).iterdir()]),
+            (templates_path, [str(file) for file in pathlib.Path(templates_path).iterdir()]),
+        ],
+        zip_safe=False,
+        install_requires=pathlib.Path("requirements.txt").read_text().split(),
+        author=info.AUTHOR,
+        author_email=info.EMAIL,
+        description=info.DESCRIPTION,
+        long_description=pathlib.Path("README.md").read_text(),
+        long_description_content_type="text/markdown",
+        license=info.PACKAGE_LICENSE,
+        keywords=info.KEYWORDS,
+        platforms="any",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Utilities",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Code Generators",
+            "Topic :: Software Development :: Libraries",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: Software Development :: User Interfaces",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    setup()
